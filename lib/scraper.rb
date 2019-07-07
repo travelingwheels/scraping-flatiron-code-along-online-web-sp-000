195a444d8501e118e99c09c427047621769944ef
Gary Wheeler <gkwheeler40@gmail.com>
2019-07-07 14:38:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,9 +13,8 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     
+    end
   end
-  end
-  Scraper.new.get_page
   
   def get_courses
     self.get_page.css(".post")
